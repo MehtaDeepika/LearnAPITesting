6fb2368dd57478e2ebe6e3e50810feb4e76cc6be
Deepika Mehta <deemehta@publicisgroupe.net>
2018-08-03 11:43:25 +0530

    Added RestAssured Test cases to Greeting controller

--- src/test/java/com/sapient/model/EmployeeControllerTests.java
+++ src/test/java/com/sapient/model/EmployeeControllerTests.java
@@ -1,30 +1,14 @@
 package com.sapient.model;
 
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+
 public class EmployeeControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
-    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+    public void verifyEmployeeDetails()
+    {
 
-        this.mockMvc.perform(get("/employee")).andDo(print()).andExpect(status().isOk());
     }
 }
--- src/test/java/com/sapient/model/GreetingControllerTests.java
+++ src/test/java/com/sapient/model/GreetingControllerTests.java
@@ -1,39 +1,44 @@
 package com.sapient.model;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+
 public class GreetingControllerTests {
-
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
     public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World!"));
+                baseURI = "http://localhost:8080/";
+                System.out.println("Started Test Default");
+                given().
+                when().get("greeting").
+                then().
+                statusCode(200).and().contentType(ContentType.JSON).and().body("content",equalTo("Hello, World!"));
     }
 
     @Test
     public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+                baseURI = "http://localhost:8080/";
+                System.out.println("Started Test Custom");
+                given().
+                queryParam("name","Deepika").
+                when().get("greeting").
+                then().
+                body("content",equalTo("Hello, Deepika!"));
     }
 
+    @Test
+    public void idValueForGreetingShouldReturnANumber() throws Exception {
+        baseURI = "http://localhost:8080/";
+        System.out.println("Started Test Custom");
+        given().
+                queryParam("name","Apurv").
+                when().get("greeting").
+                then().statusCode(200).and().body("id", equalTo("8"));
+    }
 }
